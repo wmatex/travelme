d0b88ad4f04a2d7a88e8e442295b79db69be3060
Matej Vavrinec <wmatex@gmail.com>
2016-01-05 22:28:42 +0100

    Initial commit with working app

--- server.js
+++ server.js
@@ -2,7 +2,9 @@
 //  OpenShift sample Node application
 var express = require('express');
 var fs      = require('fs');
-
+var request = require('request');
+var extend = require('util')._extend;
+var async  = require('async');
 
 /**
  *  Define the sample application.
@@ -11,7 +13,6 @@ var SampleApp = function() {
 
     //  Scope.
     var self = this;
-
 
     /*  ================================================================  */
     /*  Helper functions.                                                 */
@@ -25,6 +26,11 @@ var SampleApp = function() {
         self.ipaddress = process.env.OPENSHIFT_NODEJS_IP;
         self.port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
+        self.directionsAPI = 'https://maps.googleapis.com/maps/api/directions/json?';
+        self.foursquareAPI = 'https://api.foursquare.com/v2/venues/explore';
+
+        self.MAX_FSQ_REQUESTS = 5;
+
         if (typeof self.ipaddress === "undefined") {
             //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
             //  allows us to run/test the app locally.
@@ -34,24 +40,6 @@ var SampleApp = function() {
     };
 
 
-    /**
-     *  Populate the cache.
-     */
-    self.populateCache = function() {
-        if (typeof self.zcache === "undefined") {
-            self.zcache = { 'index.html': '' };
-        }
-
-        //  Local cache for static content.
-        self.zcache['index.html'] = fs.readFileSync('./index.html');
-    };
-
-
-    /**
-     *  Retrieve entry (content) from cache.
-     *  @param {string} key  Key identifying content to retrieve from cache.
-     */
-    self.cache_get = function(key) { return self.zcache[key]; };
 
 
     /**
@@ -89,21 +77,138 @@ var SampleApp = function() {
     /*  App server functions (main app logic here).                       */
     /*  ================================================================  */
 
+    function jsonRequest(url, query, success, fail) {
+      return request({
+        url: url, //URL to hit
+        qs: query, //Query string data
+        method: 'GET', //Specify the method
+        headers: { //We can define headers too
+          'Content-Type': 'application/json',
+        },
+        timeout: 1000,
+      }, function(error, response, body){
+        if(error) {
+          fail(error);
+        } else {
+          success(response, JSON.parse(body));
+        }
+      });
+    }
+
+    function foursquareRequest(options, success, fail) {
+      console.log(options);
+      var fopts = extend(options, {
+          client_id: 'GOOXRPVHTF4EWWQ3XYW0R1BENO2SNXSKTSEHCMRQHUVEFYPT',
+          client_secret: 'LCCI2TUVFRBNMY4AAUBFVTT3D4BIGJFXIRPTX425QABJMNCP',
+          v: '20160105',
+          m: 'foursquare',
+          llAcc: 1,
+          openNow: 1,
+          venuePhotos: 1
+        });
+      return jsonRequest(
+        self.foursquareAPI,
+        fopts,
+        success,
+        fail
+      );
+    }
+
+    function processVenues(callback) {
+      return function(response, data) {
+        console.log(data);
+        if (data.response.totalResults > 0) {
+          callback(null, data.response.groups[0].items);
+        } else {
+          callback(null, null);
+        }
+      }
+    }
+
     /**
      *  Create the routing table entries + handlers for the application.
      */
     self.createRoutes = function() {
-        self.routes = { };
+      self.app.get('/route/:start/:end', function(req, res) {
 
-        self.routes['/asciimo'] = function(req, res) {
-            var link = "http://i.imgur.com/kmbjB.png";
-            res.send("<html><body><img src='" + link + "'></body></html>");
-        };
+        res.setHeader('Content-Type', 'application/json');
 
-        self.routes['/'] = function(req, res) {
-            res.setHeader('Content-Type', 'text/html');
-            res.send(self.cache_get('index.html') );
-        };
+        var startPoint = req.params.start;
+        var endPoint   = req.params.end;
+
+        jsonRequest(
+          self.directionsAPI,
+          {
+            origin: startPoint,
+            destination: endPoint,
+            mode: "walking",
+          },
+          function (response, data) {
+            var steps = data.routes[0].legs[0].steps;
+            var venues = [];
+            var resp = [{
+              lat: data.routes[0].legs[0].start_location.lat,
+              lng: data.routes[0].legs[0].start_location.lng
+            }];
+            var requests = [
+              function (callback) {
+                var ll = data.routes[0].legs[0].start_location.lat + "," +
+                  data.routes[0].legs[0].start_location.lng;
+                foursquareRequest({
+                  ll: ll,
+                  radius: 100,
+                },
+                processVenues(callback),
+                function(error) {
+                  console.log(error);
+                  callback(null, error);
+                });
+              }
+            ];
+            var nOfSteps = steps.length;
+            var skip = Math.ceil((nOfSteps+1)/self.MAX_FSQ_REQUESTS);
+            if (skip < 1) skip = 1;
+            steps.forEach(function(step, index) {
+              if ((index+1) % skip == 0) {
+                requests.push(
+                  function (callback) {
+                    var ll =step.end_location.lat + "," +
+                    step.end_location.lng;
+                    foursquareRequest({
+                      ll: ll,
+                      radius: 100,
+                    },
+                    processVenues(callback),
+                    function (error) {
+                      console.log("Foursquare request error: " + error);
+                      callback(null, error);
+                    }
+                  );
+                }
+              );
+            }
+            });
+            console.log(requests.length);
+            async.parallel(
+              requests,
+              function (err, results) {
+                res.send({
+                  places: results,
+                  directions: data
+                });
+              }
+            );
+          },
+          function (error) {
+            console.log(error);
+          }
+        );
+      });
+
+      self.app.get("/", function (req, res) {
+        res.setHeader('Content-Type', 'text/html');
+        res.send(fs.readFileSync('./index.html'));
+      });
     };
 
 
@@ -112,13 +217,8 @@ var SampleApp = function() {
      *  the handlers.
      */
     self.initializeServer = function() {
-        self.createRoutes();
         self.app = express.createServer();
-
-        //  Add handlers for the app (from the routes).
-        for (var r in self.routes) {
-            self.app.get(r, self.routes[r]);
-        }
+        self.createRoutes();
     };
 
 
@@ -127,7 +227,6 @@ var SampleApp = function() {
      */
     self.initialize = function() {
         self.setupVariables();
-        self.populateCache();
         self.setupTerminationHandlers();
 
         // Create the express server and routes.
@@ -156,4 +255,3 @@ var SampleApp = function() {
 var zapp = new SampleApp();
 zapp.initialize();
 zapp.start();
-
