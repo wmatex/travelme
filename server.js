0b2518b370611ae3c441add91f4c1534250e2ece
Matej Vavrinec <wmatex@gmail.com>
2016-01-06 15:54:51 +0100

    Add debugging message

--- server.js
+++ server.js
@@ -196,6 +196,7 @@ var SampleApp = function() {
             candidates.forEach(function(c) {
               requests.push(
                 function (callback) {
+                  console.log(candidates, c);
                   foursquareRequest({
                     ll: c[0]+","+c[1],
                     radius: distance/2,
